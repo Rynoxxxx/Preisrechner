d379a15ad91f31b8eb89ea0e54add25f2ec12985
samuel.stroot <samuel.stroot@gmail.com>
2023-08-15 13:26:43 +0200

    Taschenrechner check box

--- src/Preisrechner/PreisrechnerGUI.java
+++ src/Preisrechner/PreisrechnerGUI.java
@@ -12,11 +12,50 @@ public class PreisrechnerGUI {
     private JTextField preisAnfangText;
     private JTextField anzahlText;
     private JLabel gesamtFeld;
-    private JButton ausführenButton;
+    private JButton plusButton;
     private JTextField gesamtText;
+    private JButton minusButton;
+    private JButton malButton;
+    private JButton geteiltButton;
+    private JCheckBox checkBox1;
 
+    private boolean aktion;
     public PreisrechnerGUI() {
-        ausführenButton.addActionListener(new ActionListener() {
+        aktion=false;
+        checkBox1.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+               if(checkBox1.isSelected()) {
+                   aktion=true;
+                }
+            }
+        });
+        plusButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                //werte bekommen
+                double epreis = Double.valueOf(preisAnfangText.getText());
+                int anzahl = Integer.valueOf(anzahlText.getText());
+
+                double gpreis = anzahl + epreis;
+                if (aktion = true) {
+                    gesamtText.setText(String.valueOf(gpreis));
+                }
+            }
+        });
+        minusButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                //werte bekommen
+                double epreis = Double.valueOf(preisAnfangText.getText());
+                int anzahl = Integer.valueOf(anzahlText.getText());
+
+                double gpreis = epreis - anzahl;
+                if (aktion = true) {
+                gesamtText.setText(String.valueOf(gpreis));
+            }}
+        });
+        malButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
                 //werte bekommen
@@ -24,10 +63,23 @@ public class PreisrechnerGUI {
                 int anzahl = Integer.valueOf(anzahlText.getText());
 
                 double gpreis = anzahl * epreis;
-
+                if (aktion = true) {
                 gesamtText.setText(String.valueOf(gpreis));
-            }
+            }}
         });
+        geteiltButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                //werte bekommen
+                double epreis = Double.valueOf(preisAnfangText.getText());
+                int anzahl = Integer.valueOf(anzahlText.getText());
+
+                double gpreis = epreis / anzahl;
+                if (aktion = true) {
+                gesamtText.setText(String.valueOf(gpreis));
+            }}
+        });
+
     }
 
     public static void main(String[] args) {
